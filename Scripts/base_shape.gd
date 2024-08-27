2e8917970f7f816818f8f3ed3ea3bcb44346bc81
GUTS <140621928+G-U-T-S@users.noreply.github.com>
2024-08-27 13:07:33 -0300

    animações
    melhorei a maneira que foi implementada a animação de inflar e desinflar

--- Scripts/base_shape.gd
+++ Scripts/base_shape.gd
@@ -6,14 +6,25 @@ class_name BaseShape extends RigidBody2D
 # vai ser afetado pelo inflate
 @export var texture: Node2D = null
 
+var base_scale := Vector2.ZERO
+
+
+func _enter_tree() -> void:
+	base_scale = texture.scale
+
 
 func inflate() -> void:
 	var inflate_tween: Tween = create_tween()
-	inflate_tween.tween_property(texture, "scale", Vector2(1.3, 1.3), 0.2)
+	inflate_tween.set_ease(Tween.EASE_IN_OUT)
+	inflate_tween.tween_property(
+		texture, "scale", base_scale + Vector2(0.2, 0.2), 0.2
+	)
 	inflate_tween.play()
 
 
 func deflate() -> void:
 	var deflate_tween: Tween = create_tween()
-	deflate_tween.tween_property(texture, "scale", Vector2.ONE, 0.2)
+	deflate_tween.tween_property(
+		texture, "scale", base_scale, 0.2
+	)
 	deflate_tween.play()
